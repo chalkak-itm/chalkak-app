b12f58d7aa1218f2fb75aa18e6a77bfb60c8feb7
Baikseungwoo <swbaik01@naver.com>
2025-11-19 19:50:44 +0900

    Add some actions(animations) for chaking_to_start function

--- app/src/main/java/com/example/chalkak/HomeFragment.kt
+++ app/src/main/java/com/example/chalkak/HomeFragment.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 
@@ -16,6 +19,9 @@ class HomeFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var txtWelcome: TextView
+    private var shakeEnabled = false
+    private lateinit var layoutShakeIcon: LinearLayout
+    private lateinit var imgShakeToggle: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,6 +43,10 @@ class HomeFragment : Fragment() {
         // Initialize views
         txtWelcome = view.findViewById(R.id.txtWelcome)
 
+        // Initialize shaking icon views
+        layoutShakeIcon = view.findViewById(R.id.layoutShakeIcon)
+        imgShakeToggle = view.findViewById(R.id.imgShakeToggle)
+
         // Update welcome message with nickname
         updateWelcomeMessage()
 
@@ -57,6 +67,26 @@ class HomeFragment : Fragment() {
                 "quiz"
             )
         }
+        // Shaking ON/OFF Button action
+        layoutShakeIcon.setOnClickListener {
+            shakeEnabled = !shakeEnabled  // Toggle
+
+            if (shakeEnabled) {
+                // ON
+                layoutShakeIcon.setBackgroundResource(R.drawable.bg_shake_icon_on)
+
+                showCenterToast("\uD83D\uDD25Shake-to-start feature is now enabled!")
+
+            } else {
+                // OFF
+                layoutShakeIcon.setBackgroundResource(R.drawable.bg_shake_icon_off)
+
+                showCenterToast("Shake-to-start feature is now disabled.")
+
+            }
+
+        }
+
     }
 
     override fun onResume() {
@@ -77,6 +107,20 @@ class HomeFragment : Fragment() {
         }
         
         txtWelcome.text = "Welcome back,\n$nickname"
+    }
+    //function for setting the toast view
+    private fun showCenterToast(message: String) {
+        val inflater = layoutInflater
+        val view = inflater.inflate(R.layout.custom_toast, null)
+
+        val txt = view.findViewById<TextView>(R.id.txtToastMessage)
+        txt.text = message
+
+        val toast = Toast(requireContext())
+        toast.duration = Toast.LENGTH_SHORT
+        toast.view = view
+        toast.setGravity(Gravity.CENTER, 0, 0)
+        toast.show()
     }
 }
 
